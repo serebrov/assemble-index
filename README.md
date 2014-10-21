bc45fd0b5e2da5df0abace4080db85c2d723fd77
Boris Serebrov <serebrov@gmail.com>
2014-10-22 00:41:38 +0300

    Add initial implementation

---
+++ README.md
@@ -0,0 +1,64 @@
+Plugin for assemble.io to generate an index page with pagination.
+
+Something like this:
+
+Page 1
+-------
+- Post 1
+- Post 2
+- Post 3
+[-1- 2 3]
+
+Page 2
+------
+- Post 3
+- Post 4
+- Post 5
+[1 -2- 3]
+
+Configuration:
+1. Add 'assemble-index' to plugins
+2. Add a plugin configuration to options:
+
+    taskname: {
+        options: {
+            collections: [{
+                name: 'pages',
+                sortby: 'data.start_time',
+                sortorder: 'desc'
+            }],
+            plugins: ['assemble-index'],
+            index: {
+                //template for the index page
+                template: 'src/templates/pages/my_index.hbs',
+                //limit of records on the each page
+                limit: 2,
+                //destination folder
+                //plugin will save files as
+                //dest/path/index.html, dest/path-page2/index.html, etc
+                dest: '<%= site.dest %>/posts'
+            }
+        },
+    }
+
+Note: at the moment there are no defaults, so all parameters are required.
+
+3. Add a template for index page, example:
+
+    <ul>
+      {{#each pages}}
+          <li><a href="{{relative ../../page.dest this.dest}}">
+            <li>{{json filename }} - {{ data.headline }}</li>
+          </a></li>
+      {{/each}}
+    </ul>
+
+    <ul>
+      {{#each indexPages}}
+          <li><a href="{{relative ../page.dest this.page.dest}}">{{ indexPage }}</a>
+          </li>
+      {{/each}}
+    </ul>
+
+Here 'pages' refer to the limited subset of original pages and 'indexPages' are index pages itself,
+so it is possible to build a pagination to move between index pages.
---
+++ index.js
@@ -0,0 +1,105 @@
+'use strict';
+
+var options = {
+  stage: 'render:post:pages'
+};
+
+
+var plugin = function(params, next) {
+
+  var path = require('path');
+  var matter = require('gray-matter');
+  var _ = require('lodash');
+  var assemble = params.assemble;
+  var grunt = assemble.grunt;
+  var file = grunt.file;
+
+  var injectBody = function(layout, body) {
+    return layout.replace(assemble.engine.bodyRegex, body);
+  };
+  var processContext = function(grunt, context, data) {
+    grunt.config.data = _.extend({}, grunt.config.data, context, data);
+    return grunt.config.process(data || context);
+  };
+
+  var indexPages = [];
+
+  var buildIndex = function() {
+    var pages = assemble.options.pages;
+    var template = assemble.options.index.template;
+    var ext = path.extname(template);
+    var limit = assemble.options.index.limit;
+    var destBase = assemble.options.index.dest;
+    var options  = assemble.options;
+    var start = 0;
+    var end = 0;
+    var num = 0;
+
+    while (end < pages.length) {
+        num++;
+        var dest = num <= 1 ? destBase : destBase + '-page' + num;
+        dest = dest + '/index.html';
+        end = Math.min(pages.length, (start + limit));
+        var subPages = pages.slice(start, end);
+        start += limit;
+        //render subPages
+        var layout   = _.cloneDeep(options.defaultLayout);
+        var context = _.extend({}, assemble.util.filterProperties(options), layout.data, options.data);
+        context = processContext(grunt, context);
+        var tpl = grunt.file.read(template);
+        var parsedPage = matter(tpl);
+        var currentPage = {
+            '_page': "all",
+            assets: assemble.options.assets,
+            basename: path.basename(template, ext),
+            dirname: path.dirname(dest),
+            dest: dest,
+            src: template,
+            ext: assemble.options.ext,
+            extname: assemble.options.ext,
+            filename: path.basename(dest),
+            page: parsedPage.content,
+            data: parsedPage.data,
+        };
+        currentPage.data = processContext(grunt, context, currentPage.data);
+        context.pages = subPages;
+        context.page = currentPage;
+        context.page.page = injectBody(layout.layout, context.page.page);
+        context.indexPage = num;
+        indexPages.push(context);
+    }
+  }
+  buildIndex();
+
+  var writePage = function(page) {
+    grunt.log.write('Assembling ' + (page.dest).cyan +' ');
+    // Write the file.
+    file.write(page.dest, page.content);
+    grunt.verbose.writeln('Assembled ' + (page.dest).cyan +' OK'.green);
+  }
+
+  var idx = 0, idxCount = indexPages.length;
+  var renderPage = function() {
+    var context = indexPages[idx];
+    context.indexPages = indexPages;
+    assemble.engine.render(context.page.page, context, function (err, content) {
+      if (err) {
+        next(err);
+      }
+      indexPages[idx].page.content = content;
+      writePage(indexPages[idx].page);
+      idx++;
+      if (idx < idxCount) {
+        renderPage();
+      } else {
+        next(null, indexPages);
+      }
+    });
+  };
+  renderPage();
+
+};
+
+// export options
+plugin.options = options;
+module.exports = plugin;
